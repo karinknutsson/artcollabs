eba6f7269ca18807ce206d8b1555e2a9264f5590
Felix Worringen <felixwor@aol.com>
2021-02-18 21:05:18 +0100

    finished the chats

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,2 +1,3 @@
 class Chatroom < ApplicationRecord
+  has_many :direct_messages
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_194440) do
+ActiveRecord::Schema.define(version: 2021_02_18_195128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,7 +73,7 @@ ActiveRecord::Schema.define(version: 2021_02_16_194440) do
   end
 
   create_table "messages", force: :cascade do |t|
-    t.string "content"
+    t.text "content"
     t.bigint "project_chat_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
