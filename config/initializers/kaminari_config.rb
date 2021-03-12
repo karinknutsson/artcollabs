894447533f8d7cc5e4cfb55d57080596e572444c
karin knutsson <karinmiriam@gmail.com>
2021-03-12 18:42:45 +0100

    default changed to 33 for faster load

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Kaminari.configure do |config|
-  config.default_per_page = 36
-  config.max_per_page = 36
+  config.default_per_page = 33
+  config.max_per_page = 33
   # config.window = 4
   # config.outer_window = 0
   # config.left = 0
