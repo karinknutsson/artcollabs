0e7b211e8b3d39fd7c322292df29db78678af2f0
karin knutsson <karinmiriam@gmail.com>
2021-05-13 12:44:17 +0200

    redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,13 +110,15 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_cable.url = "ws://www.artcollabs.net/cable"
-  config.action_cable.allowed_request_origins = [ "http://www.artcollabs.net", "https://www.artcollabs.net" ]
+  config.action_cable.url = "wss://art-collabs.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = [ "http://art--collabs.herokuapp.com", "https://art--collabs.herokuapp.com" ]
 
   # Adds files to media without overwriting
   config.active_storage.replace_on_assign_to_many = false
 
-  # config.cache_store = :redis_cache_store, { url: ENV.fetch("REDISCLOUD_URL", "redis://localhost:6379/1") }
-  config.cache_store = :redis_cache_store, { url: ENV.fetch("REDISCLOUD_URL", "redis://default:HTrniZMBJPSngtCxV5vDzz58Dp4qKBK4@redis-13674.c77.eu-west-1-1.ec2.cloud.redislabs.com:13674") }
+  Rails.application.configure do
+    config.cache_store = :redis_cache_store, { url: ENV.fetch("REDISCLOUD_URL", "redis://localhost:6379/0") }
+    # config.cache_store = :redis_cache_store, { url: ENV.fetch("REDISCLOUD_URL", "redis://default:HTrniZMBJPSngtCxV5vDzz58Dp4qKBK4@redis-13674.c77.eu-west-1-1.ec2.cloud.redislabs.com:13674") }
+  end
 
 end
