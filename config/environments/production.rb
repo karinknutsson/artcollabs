021e376506b17a0bd4c14021b58e5fc7bbeb39a0
karin knutsson <karinmiriam@gmail.com>
2022-02-18 14:19:11 +0100

    replaced redis cloud with redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,7 +118,7 @@ Rails.application.configure do
   config.active_storage.replace_on_assign_to_many = false
 
   Rails.application.configure do
-    config.cache_store = :redis_cache_store, { url: ENV.fetch("REDISCLOUD_URL", "redis://localhost:6379/1") }
+    config.cache_store = :redis_cache_store, { url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1") }
   end
 
 end
